cdd0cdf40007bcbf4d6077156a3d5f6677ee25ab
devjjin <sujink34@naver.com>
2023-04-18 17:36:33 +0900

    Refactor : TodoItem의 속성,이벤트, filter메소드를 부모 컴포넌트로 분리(#1)

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
-const TodoItem = ({ todoItem, todos, setTodos }) => {
-  const onRemoveHandler = id => setTodos(todos.filter(todo => todo.id !== id));
-
-  const onStatusHandler = id => setTodos(todos.map(todo => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+const TodoItem = ({ todoItem, onRemoveHandler, onStatusHandler}) => {
 
   return (
     <div className="todoitem__container">
@@ -11,8 +8,8 @@ const TodoItem = ({ todoItem, todos, setTodos }) => {
         <h3>{todoItem.title}</h3>
         <div>{todoItem.content}</div>
         <div className="todoitem__btn__container">
-          <button className="todoitem__removeBtn" onClick={() => onRemoveHandler(todoItem.id)}>삭제하기</button>
-          <button className="todoitem__statusBtn" onClick={() => onStatusHandler(todoItem.id)}>{!todoItem.isDone ? '완료' : '취소'}</button>
+          <button className="todoitem__removeBtn" onClick={onRemoveHandler}>삭제하기</button>
+          <button className="todoitem__statusBtn" onClick={onStatusHandler}>{!todoItem.isDone ? '완료' : '취소'}</button>
         </div>
       </div>
     </div>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,23 +2,35 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 function TodoList({ todos, setTodos }) {
+  const onRemoveHandler = id => setTodos(todos.filter(todo => todo.id !== id));
+  const onStatusHandler = id => setTodos(todos.map(todo => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)));
+
+  const workingTodos = todos.filter(todo => !todo.isDone);
+  const doneTodos = todos.filter(todo => todo.isDone);
+
   return (
     <div className="todolist__container">
       <h3>Working 🔥</h3>
       <div className="todoitem__container">
-        {todos
-          .filter(todo => !todo.isDone)
-          .map(todoItem => (
-            <TodoItem key={todoItem.id} todos={todos} setTodos={setTodos} todoItem={todoItem} />
-          ))}
+        {workingTodos.map(todoItem => (
+          <TodoItem
+            key={todoItem.id}
+            todoItem={todoItem}
+            onRemoveHandler={() => onRemoveHandler(todoItem.id)}
+            onStatusHandler={() => onStatusHandler(todoItem.id)}
+          />
+        ))}
       </div>
       <h3>Done 🎉</h3>
       <div className="todoitem__container">
-        {todos
-          .filter(todo => todo.isDone)
-          .map(todoItem => (
-            <TodoItem key={todoItem.id} todos={todos} setTodos={setTodos} todoItem={todoItem} />
-          ))}
+        {doneTodos.map(todoItem => (
+          <TodoItem
+            key={todoItem.id}
+            todoItem={todoItem}
+            onRemoveHandler={() => onRemoveHandler(todoItem.id)}
+            onStatusHandler={() => onStatusHandler(todoItem.id)}
+          />
+        ))}
       </div>
     </div>
   );
